5e2a917a5b34077322640787a058bac694370ef8
mayank-shubhankar <mayankshubhankar@gmail.com>
2020-11-03 11:26:27 +0530

    Changing picture

---
+++ build/precache-manifest.b13c9c94e689392720d5f975c532ecf0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc9f3f612602449fdff8cd396864034f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd61b2233521bd6739bd",
+    "url": "/static/css/2.afd9a251.chunk.css"
+  },
+  {
+    "revision": "e5e9aff77dad4c50bf4b",
+    "url": "/static/css/main.9bb977e7.chunk.css"
+  },
+  {
+    "revision": "dd61b2233521bd6739bd",
+    "url": "/static/js/2.cce827ae.chunk.js"
+  },
+  {
+    "revision": "e5e9aff77dad4c50bf4b",
+    "url": "/static/js/main.10a2bfde.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
